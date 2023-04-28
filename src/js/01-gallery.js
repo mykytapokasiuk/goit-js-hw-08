dec49bb86c16321e830e2433aa2fc55f23208ca0
Mykyta Pokasiuk <mykyta.pokasiuk@ukr.net>
2023-04-28 07:53:24 +0300

    Task-1 and Task-2 were complited

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,75 @@
-// Add imports above this line
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  targetElement: document.querySelector('.gallery'),
+};
+
+const variables = {
+  modal: document.createElement('div'),
+  modalBox: document.createElement('div'),
+  modalText: document.createElement('p'),
+  modalOkBtn: document.createElement('button'),
+  modalCancelBtn: document.createElement('button'),
+  modalLink: document.createElement('a'),
+};
+
+/**
+ * Creates markup for one gallery item
+ * @param {object} galleryItem
+ * @returns {string} String with markup
+ */
+const createGalleryElement = galleryItem => {
+  const { preview, original, description } = galleryItem;
+  return `
+    <li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`;
+};
+
+const createdGallery = galleryItems.map(createGalleryElement).join('');
+refs.targetElement.insertAdjacentHTML('afterbegin', createdGallery);
+
+const lightbox = new simpleLightbox('.gallery > li a', {
+  captionsData: 'alt',
+  navText: ['〈', '〉'],
+  captionDelay: 250,
+  overlayOpacity: 0.3,
+});
+
+/**
+ * Creates modal window
+ */
+const createModal = () => {
+  variables.modal.classList.add('modal', 'active');
+  variables.modalBox.classList.add('modalbox');
+  variables.modalOkBtn.classList.add('modal_btn', 'modalok_btn');
+  variables.modalCancelBtn.classList.add('modal_btn');
+  variables.modalText.textContent = 'Want to see more beautiful photos?';
+  variables.modalOkBtn.setAttribute('type', 'button');
+  variables.modalOkBtn.textContent = 'Ok';
+  variables.modalCancelBtn.setAttribute('type', 'button');
+  variables.modalCancelBtn.textContent = 'Cancel';
+  variables.modalLink.setAttribute('href', 'https://pixabay.com/');
+  variables.modalLink.setAttribute('target', '_blank');
+  variables.modalLink.setAttribute('rel', 'noreferrer noopener nofollow');
+  variables.modalLink.appendChild(variables.modalOkBtn);
+  variables.modalBox.append(
+    variables.modalText,
+    variables.modalLink,
+    variables.modalCancelBtn
+  );
+  variables.modal.appendChild(variables.modalBox);
+  document.body.appendChild(variables.modal);
+  variables.modalOkBtn.addEventListener('click', () => {
+    variables.modal.remove();
+  });
+  variables.modalCancelBtn.addEventListener('click', () => {
+    variables.modal.remove();
+  });
+};
+
+lightbox.on('closed.simplelightbox', createModal);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,67 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const refs = {
+  iframe: document.querySelector('iframe'),
+  container: document.querySelector('.container'),
+};
+
+const player = new Player(refs.iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    try {
+      const dataToStorage = JSON.stringify(data.seconds);
+      localStorage.setItem('videoplayer-current-time', dataToStorage);
+    } catch (error) {
+      console.error(`${error.name} ${error.message}`);
+    }
+  }, 1000)
+);
+
+/**
+ * Creates markup for message
+ * @param {string} text
+ * @returns {string} String with markup
+ */
+const createMessage = text => {
+  return `
+  <div class="message-box">
+  <p class="message-text">${text}</p>
+</div>
+`;
+};
+
+/**
+ * Sets the playback time for the player from the previous browser session, shows a window with this time
+ */
+const onPageLoad = () => {
+  try {
+    const dataFromStorage = localStorage.getItem('videoplayer-current-time'),
+      parsedData = JSON.parse(dataFromStorage);
+    player
+      .setCurrentTime(parsedData)
+      .then(seconds => {
+        const minutes = Math.floor(seconds / 60),
+          sec = Math.floor(seconds - minutes * 60),
+          createdMessage = createMessage(
+            `Video was stopped at ${minutes}.${sec}`
+          );
+        refs.container.insertAdjacentHTML('afterbegin', createdMessage);
+
+        const messageBoxRef = document.querySelector('.message-box ');
+        messageBoxRef.classList.toggle('active');
+        setTimeout(() => {
+          messageBoxRef.classList.toggle('active');
+        }, 4000);
+      })
+      .catch(error => {
+        console.error(`${error.name} ${error.message}`);
+      });
+  } catch (error) {
+    console.error(`${error.name} ${error.message}`);
+  }
+};
+
+window.addEventListener('load', onPageLoad);
